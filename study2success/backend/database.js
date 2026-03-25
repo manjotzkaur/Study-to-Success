fda42161f2ce95a4d1fd55192d520ef46fcd429f
manjotzkaur <manjotzkaur00@gmail.com>
2026-03-26 00:48:59 +0530

    Update database.js

--- study2success/backend/database.js
+++ study2success/backend/database.js
@@ -2,17 +2,23 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10, // adjust as needed
+  queueLimit: 0
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) console.error('❌ MySQL connection error:', err);
-  else console.log('✅ Connected to Railway MySQL!');
+  else {
+    console.log('✅ Connected to Railway MySQL!');
+    if (connection) connection.release();
+  }
 });
 
-module.exports = connection;
+module.exports = pool;
